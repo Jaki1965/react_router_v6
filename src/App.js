6b79d53acde78651b0020887b39cb0337da0e131
Jaki1965 <103109296+Jaki1965@users.noreply.github.com>
2023-06-13 01:41:53 +0300

    Revert "Lesson 2"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {Homepage} from './pages/Homepage';
 import {Aboutpage} from './pages/Aboutpage';
 import {Blogpage} from './pages/Blogpage';
 import {Notfoundpage} from './pages/Notfoundpage';
-import {Layout} from './components/Layout';
 
 
 
@@ -12,13 +11,16 @@ import {Layout} from './components/Layout';
 function App() {
   return (
     <>
+      <header>
+        <Link to = '/'>Home</Link>
+        <Link to ='/posts'>Blog</Link>
+        <Link  to ='/about'>About</Link>
+      </header>
      <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Homepage />}/>
-        <Route path='posts' element={<Blogpage />}/>
-        <Route path='about' element={<Aboutpage />}/>
-        <Route path='*' element={<Notfoundpage />}/>
-      </Route>
+      <Route path='/' element={<Homepage />}/>
+      <Route path='/posts' element={<Blogpage />}/>
+      <Route path='/about' element={<Aboutpage />}/>
+      <Route path='*' element={<Notfoundpage />}/>
      </Routes>
     </>
   );
